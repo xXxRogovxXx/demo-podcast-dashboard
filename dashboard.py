1a70ece1925ac77a73d266da52ff753d3589002b
xXxRogovxXx <104319199+xXxRogovxXx@users.noreply.github.com>
2026-07-07 13:22:40 +0300

    +стили

--- dashboard.py
+++ dashboard.py
@@ -347,6 +347,16 @@ st.markdown("""
         font-size: 1.2rem;
         font-weight: bold;
     }
+
+        /* Стили для label radio button */
+    div[data-testid="stRadio"] > label {
+        color: #ffffff !important;
+        font-size: 1.2rem !important;
+        font-weight: 700 !important;
+        text-shadow: 0 0 20px rgba(240, 147, 251, 0.6) !important;
+        margin-bottom: 0.5rem !important;
+    }
+    
 </style>
 """, unsafe_allow_html=True)
 
